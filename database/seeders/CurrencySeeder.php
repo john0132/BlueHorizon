eabdca7878370039afa627433875e4a43ec057e9
John Osama <john.osama.baky@gmail.com>
2025-09-10 19:26:51 +0300

    create currency seeder

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Currency;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $currencies = [
+            ['code' => 'EGP', 'symbol' => 'E£', 'rate' => 1, 'is_active' => true],
+            ['code' => 'USD', 'symbol' => '$',  'rate' => 1, 'is_active' => true],
+            ['code' => 'EUR', 'symbol' => '€',  'rate' => 1, 'is_active' => true],
+        ];
+
+        foreach ($currencies as $cur) {
+            Currency::updateOrCreate(
+                ['code' => $cur['code']],
+                [
+                    'symbol' => $cur['symbol'],
+                    'rate' => $cur['rate'],
+                    'is_active' => $cur['is_active'],
+                ]
+            );
+        }
+    }
+}
